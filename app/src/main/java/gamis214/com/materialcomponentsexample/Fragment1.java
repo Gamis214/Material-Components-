3210e6dc5afed64232070d03155673d3e8a5b49d
Ivan Hernandez <gamis214@gmail.com>
2019-02-28 12:58:28 -0600

    agrego de fragments para agregar mas components a las vistas, agrego de funcion de cambio de bottom para nuevo fragment

---
+++ app/src/main/java/gamis214/com/materialcomponentsexample/Fragment1.java
@@ -0,0 +1,42 @@
+package gamis214.com.materialcomponentsexample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Fragment1 extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    public static Fragment1 newInstance() {
+        Bundle args = new Bundle();
+        Fragment1 fragment = new Fragment1();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1,container,false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(new CustomAdapter());
+    }
+}
--- app/src/main/java/gamis214/com/materialcomponentsexample/MainActivity.java
+++ app/src/main/java/gamis214/com/materialcomponentsexample/MainActivity.java
@@ -1,6 +1,9 @@
 package gamis214.com.materialcomponentsexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,14 +12,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private BottomAppBar bottomAppBar;
-    private RecyclerView recyclerView;
     private FloatingActionButton fab;
     private DrawerMenu drawerMenu;
 
@@ -24,21 +27,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomAppBar = findViewById(R.id.bottom_app_bar);
-        recyclerView = findViewById(R.id.recyclerView);
+        bottomAppBar      = findViewById(R.id.bottom_app_bar);
         fab          = findViewById(R.id.fab);
         setSupportActionBar(bottomAppBar);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(new CustomAdapter());
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        fab.setOnClickListener(this);
         drawerMenu = new DrawerMenu();
+        replaceFragment(Fragment1.newInstance());
     }
 
     @Override
@@ -55,5 +49,42 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.fab:
+                fab.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                    @Override
+                    public void onShown(FloatingActionButton fab) {
+                        super.onShown(fab);
+                    }
+
+                    @Override
+                    public void onHidden(FloatingActionButton fab) {
+                        super.onHidden(fab);
+                        if (bottomAppBar.getFabAlignmentMode() == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER){
+                            bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_END);
+                            bottomAppBar.replaceMenu(R.menu.menu_home_2);
+                            replaceFragment(Fragment2.newInstance());
+                        }
+                        else {
+                            bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_CENTER);
+                            bottomAppBar.replaceMenu(R.menu.menu_home);
+                            replaceFragment(Fragment1.newInstance());
+                        }
+                        fab.show();
+                    }
+                });
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.containerFragment,fragment);
+        transaction.commit();
     }
 }
